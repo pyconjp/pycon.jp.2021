f473fcb06ae2036446e4ae4564359c0a58f6fd20
kingkoma <kingkoma8@gmail.com>
2021-08-16 23:49:51 +0900

    fix issue47 sponsors-logo style -> card-design

--- components/Doamins/TopPage/Sponsors.vue
+++ components/Doamins/TopPage/Sponsors.vue
@@ -14,47 +14,75 @@
     <div
       class="flex flex-col items-center justify-center pb-20 text-2xl font-bold  sponsors-background"
     >
-      <div class="pt-14">
-        <p class="mb-10 text-center">Platinum</p>
-        <div class="text-items-list">
-          <a
+      <div class="container pt-14">
+        <p class="text-center">Platinum</p>
+        <div class="img-items-list">
+          <div
             v-for="sponsor in platinumSponsorsList"
             :key="sponsor.nameJp"
-            :href="sponsor.link"
-            target="_blank"
-            rel="noopener noreferrer"
-            :class="{ 'pointer-events-none': sponsor.link === '#' }"
+            class="w-full p-2 my-1 lg:w-1/3"
           >
-            <p
-              v-if="sponsor.link === '#'"
-              class="m-2 text-3xl break-all lg:text-4xl"
+            <a
+              :href="sponsor.link"
+              target="_blank"
+              rel="noopener noreferrer"
+              :class="{ 'pointer-events-none': sponsor.link === '#' }"
             >
-              {{ sponsor.nameJp }}
-            </p>
-            <img
-              v-else
-              :src="require(`@/assets/images/${sponsor.imgURL}`)"
-              :alt="sponsor.nameJp"
-            />
-          </a>
+              <p
+                v-if="sponsor.imgURL === ''"
+                class="m-2 text-3xl text-center break-all lg:text-4xl"
+              >
+                {{ sponsor.nameJp }}
+              </p>
+              <div v-else class="p-3 lg:p-6 v-card v-sheet theme-light">
+                <div class="z-0 v-responsive">
+                  <div style="padding-bottom: 66.6667%"></div>
+                  <div
+                    class="bg-contain v-image-image"
+                    v-bind:style="{
+                      backgroundImage:
+                        'url(' +
+                        require(`@/assets/images/${sponsor.imgURL}`) +
+                        ')',
+                      backgroundPosition: 'center center',
+                    }"
+                  ></div>
+                </div>
+              </div>
+            </a>
+          </div>
         </div>
       </div>
-      <div>
+      <div class="container">
         <p class="mt-10 text-center">Gold</p>
         <div class="img-items-list">
-          <a
+          <div
             v-for="sponsor in goldSponsorsList"
             :key="sponsor.nameEn"
-            :href="sponsor.link"
-            target="_blank"
-            rel="noopener noreferrer"
-            class="w-1/2 px-2 py-8 lg:w-1/4"
+            class="w-1/2 p-2 my-1 lg:w-1/4"
           >
-            <img
-              :src="require(`@/assets/images/${sponsor.imgURL}`)"
-              :alt="sponsor.nameEn"
-            />
-          </a>
+            <a
+              :href="sponsor.link"
+              target="_blank"
+              rel="noopener noreferrer"
+              :class="{ 'pointer-events-none': sponsor.link === '#' }"
+            >
+              <div class="p-3 lg:p-6 v-card v-sheet theme-light">
+                <div class="z-0 v-responsive">
+                  <div style="padding-bottom: 66.6667%"></div>
+                  <div
+                    class="bg-contain v-image-image"
+                    v-bind:style="{
+                      backgroundImage:
+                        'url(' +
+                        require(`@/assets/images/${sponsor.imgURL}`) +
+                        ')',
+                      backgroundPosition: 'center center',
+                    }"
+                  ></div>
+                </div></div
+            ></a>
+          </div>
         </div>
       </div>
       <div>
@@ -250,5 +278,38 @@ export default {
 }
 .text-items-list {
   @apply grid grid-cols-1 gap-4 text-center px-5 lg:px-9 lg:grid-cols-3  place-items-center;
+}
+
+.v-sheet {
+  display: block;
+  border-radius: 2px;
+  position: relative;
+  transition: 0.3s cubic-bezier(0.25, 0.8, 0.5, 1);
+}
+.v-responsive {
+  position: relative;
+  overflow: hidden;
+  flex: 1 0 auto;
+  display: flex;
+}
+
+.v-image-image {
+  z-index: -1;
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-repeat: no-repeat;
+}
+
+.v-card {
+  box-shadow: 0 3px 1px -2px rgba(0, 0, 0, 0.2), 0 2px 2px 0 rgba(0, 0, 0, 0.14),
+    0 1px 5px 0 rgba(0, 0, 0, 0.12);
+}
+.theme-light.v-sheet {
+  background-color: #fff;
+  border-color: #fff;
+  color: rgba(0, 0, 0, 0.87);
 }
 </style>
