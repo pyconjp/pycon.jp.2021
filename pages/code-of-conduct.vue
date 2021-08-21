90ff2d481157c9e0c172ddae6323a0271b17cb1c
Hiroya Akita <akky.develop@gmail.com>
2021-08-21 16:07:45 +0900

    fix: add coc page and fix header link

---
+++ pages/code-of-conduct.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <CustomHeader />
+    <div>
+      <p
+        class="-mb-8 font-bold text-center  text-shadow text-7xl lg:text-9xl lg:-mb-14"
+      >
+        Sponsors
+      </p>
+      <p class="text-4xl font-bold text-center lg:text-6xl text-blue-green">
+        Sponsors
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+import CustomHeader from '../components/Doamins/Header.vue'
+
+export default {
+  components: { CustomHeader },
+}
+</script>
+
+<style></style>
